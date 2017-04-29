1dc2f707e293ffde7556c771ba22bb325fd63a62
jbyall <jpb5002@gmail.com>
2017-04-29 13:19:22 -0600

    Added project for domain models

---
+++ PieShop/Domain/Pie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Pie
+    {
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}

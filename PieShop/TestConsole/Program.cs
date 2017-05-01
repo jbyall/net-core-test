bf8d792d2cbfde458e409b80d64415356340720e
jbyall <jpb5002@gmail.com>
2017-04-30 22:58:05 -0600

    Added test console app to demonstrate how configuration manager has changed.

---
+++ PieShop/TestConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace TestConsole
+{
+    class Program
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+        static void Main(string[] args)
+        {
+            // Use the app settings configuration file
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            // Read the string from the appsettings.json file
+            Console.WriteLine($"{Configuration["Greeting"]}");
+            Console.ReadKey(true);
+        }
+    }
+}
